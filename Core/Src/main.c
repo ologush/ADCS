f2348b414e34f5709ee750b1dbfdf82ab7ff3142
ologush <17ojl@queensu.ca>
2025-12-01 21:41:25 -0500

    Added ISM330bx library to CMAKELists for build

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ism330bx_reg.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
